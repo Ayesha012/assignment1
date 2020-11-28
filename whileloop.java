3928912be096a1c5ba484fdc2b03a18546d1aed7
Ayesha012 <97duafatima@gmail.com>
2020-11-28 20:46:57 +0500

    while loop

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+//while structure
+public class whileloop {  
+public static void main(String[] args) {  
+    int i=1;  
+    while(i<=10){  
+        System.out.println("Stay home stay safe");  
+    i++;  
+    }  
+}  
+}  
