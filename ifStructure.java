4abcb76e09416913ea9d45e6addb2cb815e9bc6b
Ayesha012 <97duafatima@gmail.com>
2020-11-28 20:40:19 +0500

    if-else

---
+++ ifStructure.java
@@ -0,0 +1,14 @@
+       
+       // If-else structure  
+public class ifStructure{
+    public static void main(String[] args) {
+        int n =Integer.parseInt(args[0]);
+        if (n%2==0) {
+        System.out.println("Number is even");
+        }
+        else{
+            System.out.println("Number is even");
+        }
+        
+    }
+}
